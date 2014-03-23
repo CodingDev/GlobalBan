eb20605a890fa358e497f609569eca430703b827
R3N3PDE <rene.preuss@hostinfin.net>
2014-03-23 18:56:04 +0100

    Init Project

---
+++ src/de/CodingDev/GlobalBan/GlobalBan.java
@@ -0,0 +1,13 @@
+package de.CodingDev.GlobalBan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GlobalBan extends JavaPlugin{
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+}
