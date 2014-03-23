c421b4f71793d4541a812f7b1ad800ed0344ae7f
R3N3PDE <rene.preuss@hostinfin.net>
2014-03-23 19:24:02 +0100

    Create basic classes.

--- src/de/CodingDev/GlobalBan/GlobalBan.java
+++ src/de/CodingDev/GlobalBan/GlobalBan.java
@@ -3,11 +3,29 @@ package de.CodingDev.GlobalBan;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GlobalBan extends JavaPlugin{
+	
+	/*
+	 * Code Cleanup from		: 23.03.2014 19:00 
+	 * Authors					: R3N3PDE
+	 * Website from R3N3PDE 	: http://r3n3p.de
+	 * Website from CodingDev	: http://codingdev.de
+	 * Website from GlobalBan 	: http://globalban.net 
+	 * 
+	 * */
+	
+	public PlayerChecker playerChecker;
+	public GlobalBanServer globalBanServer;
+	
 	public void onEnable(){
-		
+		globalBanServer = new GlobalBanServer(this);
+		globalBanServer.checkAPIKeys();
+		playerChecker = new PlayerChecker(this);
+		getServer().getPluginManager().registerEvents(new GlobalBanEvents(this), this);
+		playerChecker.startupCheck();
+		getLogger().info("GlobalBan enabled.");
 	}
 	
 	public void onDisable(){
-		
+		getLogger().info("GlobalBan disabled.");
 	}
 }
